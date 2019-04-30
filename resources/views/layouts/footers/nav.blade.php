326e9f9d3dd0bd2ce50dee6a27b67ea834700819
ftg <ftg@SYNALOCKs-iMac.local>
2019-04-30 10:18:22 +0300

    Initial commit

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://www.oaknetbusiness.com" class="font-weight-bold ml-1" target="_blank">Oaknet business</a>
+            &copy; {{ now()->year }} <a href="https://www.fluidtechglobal.com" class="font-weight-bold ml-1" target="_blank">Fluidtech Global</a>
         </div>
     </div>
     <div class="col-xl-6">
