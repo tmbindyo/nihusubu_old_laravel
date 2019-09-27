a6ef415912a84bffd3c4ae4c1a03fc9b21b117c8
tomulumbi <tmbindyo@fluidtechglobal.com>
2019-09-27 11:41:53 +0300

    Creating mcr for institution services

---
+++ app/Http/Controllers/InstitutionServiceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InstitutionService;
+use Illuminate\Http\Request;
+
+class InstitutionServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InstitutionService  $institutionService
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InstitutionService $institutionService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InstitutionService  $institutionService
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InstitutionService $institutionService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InstitutionService  $institutionService
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InstitutionService $institutionService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InstitutionService  $institutionService
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InstitutionService $institutionService)
+    {
+        //
+    }
+}
