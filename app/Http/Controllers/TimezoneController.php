9e98ef9f08b517961e96a0a14ca31e988394debd
tomulumbi <tmbindyo@fluidtechglobal.com>
2019-09-27 11:47:43 +0300

    Creating mcr for timezones

---
+++ app/Http/Controllers/TimezoneController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Timezone;
+use Illuminate\Http\Request;
+
+class TimezoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Timezone  $timezone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Timezone $timezone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Timezone  $timezone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Timezone $timezone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Timezone  $timezone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Timezone $timezone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Timezone  $timezone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Timezone $timezone)
+    {
+        //
+    }
+}
