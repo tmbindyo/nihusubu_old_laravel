a5d197c904e7d3e073bf0104e3ba2277fa66d94e
unknown <themichael.01.wanyoike@gmail.com>
2019-11-18 12:23:49 +0300

    Removal of rows on orders.

--- resources/views/business/order_create.blade.php
+++ resources/views/business/order_create.blade.php
@@ -289,7 +289,7 @@
             itemQuantity[0].setAttribute("max", selectedItemQuantity);
         };
         itemRate[0].value = selectItemPrice;
-    }
+    };
     function changeItemQuantity (e) {
         var quantityValue;
         if (e.value.isEmpty) {
@@ -309,7 +309,7 @@
         };
         itemTotalInputField[0].value = quantityValue * itemRate;
         itemTotalChange();
-    }
+    };
     function changeItemRate (e) {
         var subTotal = [];
         var adjustedValue;
@@ -331,7 +331,7 @@
         };
         itemTotalInputField[0].value = quantityValue * itemRate;
         itemTotalChange();
-    }
+    };
     function addTableRow () {
         var table = document.getElementById("estimate_table");
         var row = table.insertRow();
@@ -355,7 +355,15 @@
         secondCell.innerHTML = "<input oninput = 'changeItemQuantity(this)' name='item_details[0][quantity]' type='number' class='form-control input-lg item-quantity' value = '0' min = '0'>";
         thirdCell.innerHTML = "<input oninput = 'changeItemRate(this)' name='item_details[0][rate]' type='number' class='form-control input-lg item-rate' placeholder='E.g +10, -10' value = '0' min = '0'>";
         fourthCell.innerHTML = "<input name='item_details[0][amount]' type='number' class='form-control input-lg item-total' placeholder='E.g +10, -10' value = '0' min = '0'>";
-    }
+        fifthCell.innerHTML = "<span><i onclick = 'removeSelectedRow(this)' class = 'fa fa-minus-circle btn btn-danger'></i></span>";
+        fifthCell.setAttribute("style", "width: 1em;")
+    };
+    function removeSelectedRow (e) {
+        var selectedParentTd = e.parentElement.parentElement;
+        var selectedTr = selectedParentTd.parentElement;
+        var selectedTable = selectedTr.parentElement;
+        selectedTable.removeChild(selectedTr);
+    };
     function itemTotalChange () {
         subTotal = [];
         var itemTotals = document.getElementsByClassName("item-total");
@@ -374,6 +382,6 @@
         document.getElementById("adjustment-text").innerHTML = adjustedValue;
         var adjustedTotal = Number(adjustedValue) + Number(itemSubTotal);
         document.getElementById("grand-total").innerHTML = adjustedTotal;
-    }
+    };
 </script>
 @endsection
