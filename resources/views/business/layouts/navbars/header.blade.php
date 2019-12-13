acd47c3a7e144ade77a6af3b55834ff15b3d7e6f
tomulumbi <tmbindyo@fluidtechglobal.com>
2019-12-13 16:02:02 +0300

    Logout and head bar

--- resources/views/business/layouts/navbars/header.blade.php
+++ resources/views/business/layouts/navbars/header.blade.php
@@ -2,17 +2,17 @@
     <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
             <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-            <form role="search" class="navbar-form-custom" action="search_results.html">
+            {{--  <form role="search" class="navbar-form-custom" action="search_results.html">
                 <div class="form-group">
                     <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
                 </div>
-            </form>
+            </form>  --}}
         </div>
         <ul class="nav navbar-top-links navbar-right">
             <li>
                 <span class="m-r-sm text-muted welcome-message">Welcome to Nihusubu.</span>
             </li>
-            <li class="dropdown">
+            {{--  <li class="dropdown">
                 <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
                     <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
                 </a>
@@ -106,18 +106,21 @@
                         </div>
                     </li>
                 </ul>
-            </li>
+            </li>  --}}
 
             <li>
-                <a href="{{route('logout')}}">
+                <a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <i class="fa fa-sign-out"></i> Log out
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
             </li>
-            <li>
+            {{--  <li>
                 <a class="right-sidebar-toggle">
                     <i class="fa fa-tasks"></i>
                 </a>
-            </li>
+            </li>  --}}
         </ul>
 
     </nav>
