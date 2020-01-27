c5b2e3a0516b287b19ce80be785308d1d123f3aa
tomulumbi <tmbindyo@fluidtechglobal.com>
2020-01-27 16:28:29 +0300

    forgotten password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -139,9 +139,9 @@
                 @endif
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                <a href="{{route('business.forgot.password')}}"><small>Forgot password?</small></a>
+                <a href="{{route('password.request')}}"><small>Forgot password?</small></a>
                 <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="{{route('business.register')}}">Create an account</a>
+                <a class="btn btn-sm btn-white btn-block" href="{{route('register')}}">Create an account</a>
             </form>
             <p class="m-t">Copyright &copy; <script>document.write(new Date().getFullYear());</script></p>
         </div>
